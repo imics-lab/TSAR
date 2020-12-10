9a1ecb1bdcaa9b93ea084a90ab2158807d8dae94
Gentry Atkinson <gma23@txstate.edu>
2020-12-10 10:32:40 -0600

    silly test file

---
+++ src/utils/bokeh_test.py
@@ -0,0 +1,10 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 10 December, 2020
+#Just a toy file to figure out bokeh
+
+from bokeh.plotting import figure, output_file, show
+import numpy as np
+
+if __name__ == "__main__":
+    X = np.arange(0, 360, 0.1)
