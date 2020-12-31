7d74f2495351e31ae3fc4262402ec00e3fd8b617
Gentry Atkinson <gma23@txstate.edu>
2020-12-30 20:30:26 -0600

    trying some new models

---
+++ src/tsar_test_add_models.py
@@ -0,0 +1,6 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 30 December, 2020
+#Expand the tsar test to additional models using the features sets from "tsar test"
+
+if __name__ == "__main__":
