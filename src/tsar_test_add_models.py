9a8e3c507341eb4da1e39a533f074330d9998276
Gentry Atkinson <gma23@txstate.edu>
2020-12-30 21:15:50 -0600

    added model list

--- src/tsar_test_add_models.py
+++ src/tsar_test_add_models.py
@@ -3,4 +3,21 @@
 #Data: 30 December, 2020
 #Expand the tsar test to additional models using the features sets from "tsar test"
 
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+#Models to investigate:
+#   KNN
+#   C4.5
+#   Naive Bayes
+
 if __name__ == "__main__":
+    unimib_feats_noisy = np.genfromtxt("test/visualizations_for_unimib_sup/UniMiB_sup_feat.csv", delimiter=',')
+    unimib_bad_guys =  np.genfromtxt("test/visualizations_for_unimib_sup/UniMiB_identified_bad.csv", delimiter=',')
+    unimib_feats_clean = np.delete(unimib_feats_noisy, unimib_bad_guys, 0)
+    log = open("data_cleaning_experiments_results.txt")
+
+    log.write("------- UniMiB Experiments-----------")
+
+    unimib_noisy_knn = KNeighborsClassifier(n_neighbors=3)
+    unimib_clean_knn = KNeighborsClassifier(n_neighbors=3)
