db7358e8bd66f22fd95bd9b04e8dd5ab6355ba52
Gentry Atkinson <gma23@txstate.edu>
2021-01-02 15:52:06 -0600

    who loves confusion matrixes

---
+++ src/utils/generate_tsar_matrix.py
@@ -0,0 +1,7 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 02 January, 2021
+#Produce several confusion matrix based on the TSAR test results
+#Hurray! First code file of the new year
+
+import matplotlib.pyplot as plt
