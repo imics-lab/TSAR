d914a94f1938ea3ff84b4bc5aab8f5a9a026ede7
Gentry Atkinson <gma23@txstate.edu>
2021-01-02 16:18:56 -0600

    generated some cool matrices

--- src/utils/generate_tsar_matrix.py
+++ src/utils/generate_tsar_matrix.py
@@ -5,3 +5,52 @@
 #Hurray! First code file of the new year
 
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+
+#credit to https://onestopdataanalysis.com/confusion-matrix-python/ for a handy func
+def plot_confusion_matrix(data, labels, output_filename):
+    """Plot confusion matrix using heatmap.
+
+    Args:
+        data (list of list): List of lists with confusion matrix data.
+        labels (list): Labels which will be plotted across x and y axis.
+        output_filename (str): Path to output file.
+
+    """
+    seaborn.set(color_codes=True)
+    plt.figure(1, figsize=(9, 6))
+
+    plt.title("")
+
+    seaborn.set(font_scale=1.4)
+    ax = seaborn.heatmap(data, annot=True, cmap="YlGnBu", cbar_kws={'label': 'Scale'}, fmt='g')
+
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+
+    ax.set(ylabel="Reviewer Response", xlabel="True Value")
+
+    plt.savefig(output_filename, bbox_inches='tight', dpi=300)
+    plt.close()
+
+if __name__ == "__main__":
+    test = [[1., 2.], [3., 4.]]
+    total_ind_responses = np.array([[407, 158], [313, 562]], dtype='int32')
+    total_vote__responses = [[30,5],[18,43]]
+    deep_ind_responses = [[286, 105],[194,375]]
+    deep_vote_responses =[[23, 3], [9,29]]
+    sup_ind_responses = [[160, 50], [80,190]]
+    sup_vote_responses = [[14, 1], [2, 15]]
+    unsup_ind_responses = [[126, 55], [114, 185]]
+    unsup_vote_responses = [[9,2], [7, 14]]
+
+    labels = ["Correct", "Mislabeled"]
+    plot_confusion_matrix(total_ind_responses, labels, "imgs/matrixes/total_ind_matrix.pdf")
+    plot_confusion_matrix(total_vote__responses, labels, "imgs/matrixes/total_vote_matrix.pdf")
+    plot_confusion_matrix(deep_ind_responses, labels, "imgs/matrixes/deep_ind_matrix.pdf")
+    plot_confusion_matrix(deep_vote_responses, labels, "imgs/matrixes/deep_vote_matrix.pdf")
+    plot_confusion_matrix(sup_ind_responses, labels, "imgs/matrixes/sup_ind_matrix.pdf")
+    plot_confusion_matrix(sup_vote_responses, labels, "imgs/matrixes/sup_vote_matrix.pdf")
+    plot_confusion_matrix(unsup_ind_responses, labels, "imgs/matrixes/unsup_ind_matrix.pdf")
+    plot_confusion_matrix(unsup_vote_responses, labels, "imgs/matrixes/unsup_vote_matrix.pdf")
