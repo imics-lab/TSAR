3319fc166a7c8068f55900e6bf100bf7598ace6b
Gentry Atkinson <gma23@txstate.edu>
2021-01-07 11:28:55 -0600

    trying to make a good looking model graph

--- src/tsar_test_add_NN.py
+++ src/tsar_test_add_NN.py
@@ -62,3 +62,127 @@ if __name__ == "__main__":
     log.write("Noisy: {}\n".format(noisy_acc))
     log.write("Clean: {}\n".format(clean_acc))
     log.write("===\n\n".format(clean_acc))
+
+    log.write("6-layer NN results: \n")
+    noisy_model = Sequential([
+        Input(shape=noisy_X_train[0].shape),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(32, activation='relu'),
+        Dense(num_labels, activation='softmax')
+    ])
+    clean_model = Sequential([
+        Input(shape=clean_X_train[0].shape),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(32, activation='relu'),
+        Dense(num_labels, activation='softmax')
+    ])
+    noisy_model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+    noisy_model.summary()
+    clean_model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+    clean_model.summary()
+
+    noisy_model.fit(noisy_X_train, noisy_y_train, epochs=20, verbose=1)
+    noisy_y_pred = noisy_model.predict(noisy_X_test)
+    clean_model.fit(clean_X_train, clean_y_train, epochs=20, verbose=1)
+    clean_y_pred = clean_model.predict(clean_X_test)
+
+    noisy_acc = accuracy_score(np.argmax(noisy_y_test, axis=-1), np.argmax(noisy_y_pred, axis=-1))
+    clean_acc = accuracy_score(np.argmax(clean_y_test, axis=-1), np.argmax(clean_y_pred, axis=-1))
+    log.write("Noisy: {}\n".format(noisy_acc))
+    log.write("Clean: {}\n".format(clean_acc))
+    log.write("===\n\n".format(clean_acc))
+
+    log.flush()
+
+    uci_feats_noisy = np.genfromtxt("test/visualizations_for_uci_sup/UCI_sup_feat.csv", delimiter=',')
+    uci_y_noisy = np.genfromtxt("test/visualizations_for_uci_sup/UCI_shuffeled_labels.csv", delimiter=',', dtype='int32')
+    #uci_y_noisy = np.argmax(uci_y_noisy, axis=-1)
+    uci_bad_guys =  np.genfromtxt("test/visualizations_for_uci_sup/UCI_identified_bad.csv", delimiter=',')
+
+    uci_feats_clean = np.delete(uci_feats_noisy, uci_bad_guys, 0)
+    uci_y_clean = np.delete(uci_y_noisy, uci_bad_guys, 0)
+
+    print("UCI noisy features shape: ", uci_feats_noisy.shape)
+    print("UCI noisy y shape: ", uci_y_noisy.shape)
+    print("UCI clean features shape: ", uci_feats_clean.shape)
+    print("UCI clean y shape: ", uci_y_clean.shape)
+
+    num_labels = uci_y_noisy.shape[1]
+
+    log.write("------- UCI NN Experiments-----------\n\n")
+
+    noisy_X_train, noisy_X_test, noisy_y_train, noisy_y_test = train_test_split(uci_feats_noisy, uci_y_noisy, test_size=0.2, shuffle=False)
+    clean_X_train, clean_X_test, clean_y_train, clean_y_test = train_test_split(uci_feats_clean, uci_y_clean, test_size=0.2, shuffle=False)
+
+    log.write("3-layer NN results: \n")
+    noisy_model = Sequential([
+        Input(shape=noisy_X_train[0].shape),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(num_labels, activation='softmax')
+    ])
+    clean_model = Sequential([
+        Input(shape=clean_X_train[0].shape),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(num_labels, activation='softmax')
+    ])
+    noisy_model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+    noisy_model.summary()
+    clean_model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+    clean_model.summary()
+
+    noisy_model.fit(noisy_X_train, noisy_y_train, epochs=20, verbose=1)
+    noisy_y_pred = noisy_model.predict(noisy_X_test)
+    clean_model.fit(clean_X_train, clean_y_train, epochs=20, verbose=1)
+    clean_y_pred = clean_model.predict(clean_X_test)
+
+    noisy_acc = accuracy_score(np.argmax(noisy_y_test, axis=-1), np.argmax(noisy_y_pred, axis=-1))
+    clean_acc = accuracy_score(np.argmax(clean_y_test, axis=-1), np.argmax(clean_y_pred, axis=-1))
+    log.write("Noisy: {}\n".format(noisy_acc))
+    log.write("Clean: {}\n".format(clean_acc))
+    log.write("===\n\n".format(clean_acc))
+
+    log.write("6-layer NN results: \n")
+    noisy_model = Sequential([
+        Input(shape=noisy_X_train[0].shape),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(32, activation='relu'),
+        Dense(num_labels, activation='softmax')
+    ])
+    clean_model = Sequential([
+        Input(shape=clean_X_train[0].shape),
+        Dense(128, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(32, activation='relu'),
+        Dense(num_labels, activation='softmax')
+    ])
+    noisy_model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+    noisy_model.summary()
+    clean_model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['acc'])
+    clean_model.summary()
+
+    noisy_model.fit(noisy_X_train, noisy_y_train, epochs=20, verbose=1)
+    noisy_y_pred = noisy_model.predict(noisy_X_test)
+    clean_model.fit(clean_X_train, clean_y_train, epochs=20, verbose=1)
+    clean_y_pred = clean_model.predict(clean_X_test)
+
+    noisy_acc = accuracy_score(np.argmax(noisy_y_test, axis=-1), np.argmax(noisy_y_pred, axis=-1))
+    clean_acc = accuracy_score(np.argmax(clean_y_test, axis=-1), np.argmax(clean_y_pred, axis=-1))
+    log.write("Noisy: {}\n".format(noisy_acc))
+    log.write("Clean: {}\n".format(clean_acc))
+    log.write("===\n\n".format(clean_acc))
+
+    log.flush()
+    log.close()
---
+++ src/utils/generate_model_graphs.py
@@ -0,0 +1,7 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 07 January, 2021
+#Print model graphs from keras
+
+import tensorflow as tf
+from tensorflow import keras
