c820d275828737dd14fb7c233de1d99ee08162e3
Gentry Atkinson <gma23@txstate.edu>
2021-01-07 11:40:05 -0600

    added tensorboard log

--- src/utils/generate_model_graphs.py
+++ src/utils/generate_model_graphs.py
@@ -5,3 +5,35 @@
 
 import tensorflow as tf
 from tensorflow import keras
+import tensorboard
+from build_AE import get_trained_AE
+from scipy.io import loadmat
+import numpy as np
+import datatime
+
+def get_unimib_data(s="acc"):
+    print("Loading UniMiB set ", s)
+    X_flat = loadmat("data/UniMiB-SHAR/data/" + s + "_data.mat")[s + "_data"]
+    y = loadmat("data/UniMiB-SHAR/data/" + s + "_labels.mat")[s + "_labels"][:,0]
+    if(s=="acc"):
+        labels = loadmat("data/UniMiB-SHAR/data/" + s + "_names.mat")[s + "_names"][0,:]
+    else:
+        labels = loadmat("data/UniMiB-SHAR/data/" + s + "_names.mat")[s + "_names"][:,0]
+    print("Num instances: ", len(X_flat))
+    print("Instance length: ", len(X_flat[0]))
+
+    y = np.array(y - 1)
+    X = np.zeros((len(X_flat), 3, 151), dtype='float')
+    X[:,0,0:151]=X_flat[:,0:151]
+    X[:,1,0:151]=X_flat[:,151:302]
+    X[:,2,0:151]=X_flat[:,302:453]
+    print(labels)
+    return X, y, labels
+
+if __name__ == "__main__":
+    print("Trained Autoencoder")
+    X, y, labels = get_unimib_data("adl")
+    model = get_trained_AE(X)
+
+    logdir="logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
